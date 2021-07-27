d2c66afbe368c482e0d0fc4a4c1241464a1cb270
unknown <283006603@qq.com>
2021-07-27 13:03:06 +0800

    更新网络请求库

--- app/src/main/java/com/example/practice/MyApplication.java
+++ app/src/main/java/com/example/practice/MyApplication.java
@@ -3,7 +3,6 @@ package com.example.practice;
 import android.app.Application;
 import android.content.Context;
 
-import com.example.practice.bean.tool.MyFastJsonConverter;
 import com.example.practice.network.RxHttpManager;
 import com.example.practice.service.InitializeService;
 import com.scwang.smart.refresh.footer.ClassicsFooter;
@@ -14,13 +13,6 @@ import com.scwang.smart.refresh.layout.api.RefreshHeader;
 import com.scwang.smart.refresh.layout.api.RefreshLayout;
 import com.scwang.smart.refresh.layout.listener.DefaultRefreshFooterCreator;
 import com.scwang.smart.refresh.layout.listener.DefaultRefreshHeaderCreator;
-
-import java.io.File;
-
-import rxhttp.RxHttp;
-import rxhttp.RxHttpPlugins;
-import rxhttp.wrapper.cahce.CacheMode;
-import rxhttp.wrapper.callback.IConverter;
 
 public class MyApplication extends Application{
 
@@ -55,13 +47,6 @@ public class MyApplication extends Application{
         //下面关于Rxhttp的不建议放在service里面，可能会导致monkey速度太快了，还没初始化完成就开始网络请求操作了
         RxHttpManager.init(this);
         //设置缓存目录为：Android/data/{app包名目录}/cache/RxHttpCache
-        File cacheDir = new File(getExternalCacheDir(), "RxHttpCache");
-        //设置最大缓存为10M，缓存有效时长为600秒
-        RxHttpPlugins.setCache(cacheDir, 10 * 1024 * 1024, CacheMode.REQUEST_NETWORK_FAILED_READ_CACHE,
-                10 * 60 * 1000);
-        IConverter converter = MyFastJsonConverter.create();
-        RxHttp.setConverter(converter);
-
         InitializeService.Companion.start(this);
 
 
--- app/src/main/java/com/example/practice/network/RxHttpManager.java
+++ app/src/main/java/com/example/practice/network/RxHttpManager.java
@@ -2,10 +2,16 @@ package com.example.practice.network;
 
 import android.app.Application;
 
+import com.example.practice.BuildConfig;
+import com.example.practice.bean.tool.MyFastJsonConverter;
+
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import rxhttp.RxHttp;
+import rxhttp.RxHttpPlugins;
+import rxhttp.wrapper.cahce.CacheMode;
+import rxhttp.wrapper.callback.IConverter;
 
 public class RxHttpManager{
 
@@ -21,7 +27,15 @@ public class RxHttpManager{
                 .addInterceptor(httpLoggingInterceptor).build();
         //RxHttp初始化，自定义OkHttpClient对象，非必须
         //设置debug模式，默认为false，设置为true后，发请求，过滤"RxHttp"能看到请求日志
-        RxHttp.init(client, true);
+        //设置缓存目录为：Android/data/{app包名目录}/cache/RxHttpCache
+        File cacheDir = new File(context.getExternalCacheDir(), "RxHttpCache");
+        //RxHttp初始化，自定义OkHttpClient对象，非必须
+        //设置最大缓存为10M，缓存有效时长为600秒
+        IConverter converter = MyFastJsonConverter.create();
+        RxHttpPlugins.init(client)
+                .setDebug(BuildConfig.DEBUG)
+                .setCache(cacheDir, 10 * 1024 * 1024, CacheMode.REQUEST_NETWORK_FAILED_READ_CACHE, 10 * 60 * 1000)
+                .setConverter(converter);
 
     }
 
